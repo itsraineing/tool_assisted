80ee107274abe45e3885789f0da84b0873b85471
itsraineing <7527892+itsraineing@users.noreply.github.com>
2020-01-10 23:07:52 -0800

    fix animations so they work with pause
    a day left in the marathon and theres no gameplay lmao
    
    current total: $1,563,268

--- actors/player.gd
+++ actors/player.gd
@@ -2,8 +2,26 @@ extends KinematicBody2D
 
 const UP: Vector2 = Vector2(0, -1)
 
+const SPRITE_FRAMES: int = 9
+const ANIM_IDLE_FRAMES: int = 2
+const ANIM_JUMP_FRAMES: int = 1
+const ANIM_FALL_FRAMES: int = 1
+const ANIM_LOOK_FRAMES: int = 3
+const ANIM_DIE_FRAMES: int = 2
+
+const ANIM_IDLE_FRAMETIME: int = 60
+const ANIM_RUN_FRAMETIME: int = 10
+const ANIM_JUMP_FRAMETIME: int = 0
+const ANIM_FALL_FRAMETIME: int = 0
+
+const ANIM_DIE_FRAMETIME: int = 15
+
+const ANIM_LOOK_ACTIVATE_FRAMES: int = 240
+
 enum {DIR_LEFT = -1, DIR_RIGHT = 1}
 enum {STATE_IDLE, STATE_WALKING, STATE_JUMPING, STATE_FALLING, STATE_ATTACKING, STATE_HIT, STATE_DEAD, STATE_NONE = -1}
+enum {FRAME_IDLE1, FRAME_IDLE2, FRAME_JUMP, FRAME_FALL, FRAME_LOOK1, FRAME_LOOK2, FRAME_LOOK3, FRAME_DIE, FRAME_NONE}
+enum {ANIM_IDLE = 0, ANIM_RUN = 621, ANIM_JUMP = 2, ANIM_FALL = 3, ANIM_LOOK = 4, ANIM_DIE = 7}
 
 export (float) var walk_speed = 8000.0
 export (float) var walk_acceleration = 1200.0
@@ -13,8 +31,6 @@ export (float) var in_air_acceleration = 20.0
 
 export (float) var gravity = 20000.0
 export (float) var gravity_acceleration = 500.0
-
-export (int) var idle_anim2_time = 180
 
 onready var game_master: Node = get_node("/root/game_master")
 
@@ -33,13 +49,14 @@ var last_state: int = STATE_NONE
 
 var idle_time: int = 0
 
+var current_anim: int = ANIM_IDLE
+var current_anim_frame: int = 0
+
 func _ready():
 	set_physics_process(true)
 	game_master.register_actor(self)
 
 func _physics_process(delta):
-	#if !game_master.is_paused:
-	#	handle_frame(delta)
 	pass
 
 func handle_frame(delta):
@@ -104,22 +121,73 @@ func handle_frame(delta):
 		if current_state != last_state:
 			match current_state:
 				STATE_IDLE:
-					$AnimationPlayer.play("idle")
+					current_anim = ANIM_IDLE
+					current_anim_frame = 0
 				STATE_WALKING:
-					$AnimationPlayer.play("idle", -1, 3)
+					current_anim = ANIM_RUN
+					current_anim_frame = 0
 				STATE_JUMPING:
-					$AnimationPlayer.stop()
-					$Sprite.frame = 2
+					current_anim = ANIM_JUMP
+					current_anim_frame = 0
 				STATE_FALLING:
-					$AnimationPlayer.stop()
-					$Sprite.frame = 3
-		elif idle_time >= idle_anim2_time:
-			idle_time = 0
-			$AnimationPlayer.play("idle2")
+					current_anim = ANIM_FALL
+					current_anim_frame = 0
+		elif idle_time >= ANIM_LOOK_ACTIVATE_FRAMES and current_anim != ANIM_LOOK:
+			current_anim = ANIM_LOOK
+			current_anim_frame = 0
+		
+		animate()
 		
 		# set up for next frame
 		last_velocity = velocity / delta
 		last_state = current_state
+
+func animate():
+	match current_anim:
+		ANIM_IDLE:
+			if current_anim_frame >= ANIM_IDLE_FRAMES * ANIM_IDLE_FRAMETIME:
+				current_anim_frame = 0
+			
+			if current_anim_frame < ANIM_IDLE_FRAMETIME:
+				$Sprite.frame = ANIM_IDLE
+			else:
+				$Sprite.frame = ANIM_IDLE + 1
+		ANIM_RUN:
+			if current_anim_frame >= ANIM_IDLE_FRAMES * ANIM_RUN_FRAMETIME:
+				current_anim_frame = 0
+			
+			if current_anim_frame < ANIM_RUN_FRAMETIME:
+				$Sprite.frame = ANIM_IDLE
+			else:
+				$Sprite.frame = ANIM_IDLE + 1
+		ANIM_JUMP:
+			$Sprite.frame = ANIM_JUMP
+		ANIM_FALL:
+			$Sprite.frame = ANIM_FALL
+		ANIM_LOOK:
+			if idle_time >= ANIM_LOOK_ACTIVATE_FRAMES + ANIM_IDLE_FRAMETIME * 2:
+				idle_time = 0
+				current_anim = ANIM_IDLE
+				current_anim_frame = 0
+			else:
+				if current_anim_frame >= ANIM_IDLE_FRAMETIME:
+					$Sprite.frame = ANIM_LOOK + 1
+				else:
+					match current_anim_frame:
+						33, 34, 35, 36, 45, 46, 47, 48:
+							$Sprite.frame = ANIM_LOOK + 2
+						_:
+							$Sprite.frame = ANIM_LOOK
+		ANIM_DIE:
+			if current_anim_frame >= ANIM_DIE_FRAMES * ANIM_DIE_FRAMETIME:
+				current_anim_frame = 0
+			
+			if current_anim_frame < ANIM_DIE_FRAMETIME:
+				$Sprite.frame = ANIM_DIE
+			else:
+				$Sprite.frame = ANIM_DIE + 1
+	
+	current_anim_frame += 1
 
 func get_move_input():
 	var dir = Vector2(0,0)
